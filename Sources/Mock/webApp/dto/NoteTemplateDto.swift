3baf6e17521e5742b810ef999b507a64e53264e7
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-07-14 12:45:42 +0200

    Added NoteTemplateType

--- Sources/Mock/webApp/dto/NoteTemplateDto.swift
+++ Sources/Mock/webApp/dto/NoteTemplateDto.swift
@@ -15,5 +15,11 @@ class NoteTemplateDto: Codable {
     var id: Int32?
     var name: String?
     var text: String?
+    var type: String?
     var taskTypeIds: [Int32]?
+}
+
+enum NoteTemplateType: String {
+    case general = "GENERAL"
+    case reschedule = "RESCHEDULE"
 }
