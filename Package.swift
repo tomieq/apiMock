5303897d5b8cecea9d9d6ae63d7daded1d673032
Tomasz Kucharski <tomasz.kucharski@comarch.pl>
2021-07-28 07:42:56 +0200

    Upgrade swifter to 1.5.5

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Mock",
     dependencies: [
-        .package(url: "https://github.com/tomieq/swifter.git", .upToNextMajor(from: "1.5.1"))
+        .package(url: "https://github.com/tomieq/swifter.git", .upToNextMajor(from: "1.5.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
